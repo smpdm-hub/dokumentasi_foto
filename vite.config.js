eb85751e3be70fc9e761bf80999c7d96371bdc2a
smpdm-hub <database.smpdm@gmail.com>
2026-06-23 11:26:37 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/banksoal/', // Ganti menjadi nama repository Anda
+  base: '/dokumentasi_foto/', // Ganti menjadi nama repository Anda
 })
